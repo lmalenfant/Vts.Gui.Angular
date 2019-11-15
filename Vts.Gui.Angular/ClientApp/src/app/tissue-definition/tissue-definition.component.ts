d827a003cae3a0156127af1e3da460029ee1fb55
Lisa Malenfant <lisa.malenfant@uci.edu>
2019-11-15 12:54:26 -0800

    Updated the Blood Concentration values when changing tissue type

--- Vts.Gui.Angular/ClientApp/src/app/tissue-definition/tissue-definition.component.ts
+++ Vts.Gui.Angular/ClientApp/src/app/tissue-definition/tissue-definition.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ListType } from '../shared/list-definition.model';
 import { BrainGrayMatter, BrainWhiteMatter, BreastPostMenopause, BreastPreMenopause, Custom, IntralipidPhantom, Liver, Skin } from '../tissue-definition/absorber-list';
 import { AbsorberConcentration } from './absorber-concentration.model';
@@ -18,19 +18,41 @@ export class TissueDefinitionComponent {
   @Input('absorberConcentration') absorberConcentration: Array<AbsorberConcentration>;
 
   constructor() {
+  }
 
+  ngOnInit() {
+    this.calculateBloodConcentration();
+  }
+
+  calculateBloodConcentration() {
+    let Hb = 0;
+    let HbO2 = 0;
+    this.absorberConcentration.forEach(function (val) {
+      console.log("label = " + val.label + " value = " + val.value);
+      if (val.label === "Hb") {
+        Hb = val.value;
+      }
+      if (val.label === "HbO2") {
+        HbO2 = val.value;
+      }
+    });
+    this.bloodConcentration.totalHb = Hb + HbO2;
+    this.bloodConcentration.stO2 = HbO2 / (Hb + HbO2);
+    this.bloodConcentration.bloodVolume = this.bloodConcentration.totalHb / 1E6 * 64500 / 150;
   }
 
   onChange(value) {
     console.log(this.tissueType.value);
     console.log(value);
     this.tissueType.value = value;
+    this.bloodConcentration.visible = true;
     switch (this.tissueType.value) {
       case 'Skin':
         this.absorberConcentration = Skin;
         break;
       case 'Liver':
         this.absorberConcentration = Liver;
+
         break;
       case 'BrainGrayMatter':
         this.absorberConcentration = BrainGrayMatter;
@@ -46,10 +68,14 @@ export class TissueDefinitionComponent {
         break;
       case 'IntralipidPhantom':
         this.absorberConcentration = IntralipidPhantom;
+        this.bloodConcentration.visible = false;
         break;
       case 'Custom':
         this.absorberConcentration = Custom;
         break;
+    }
+    if (this.absorberConcentration !== IntralipidPhantom) {
+      this.calculateBloodConcentration();
     }
   }
 }
