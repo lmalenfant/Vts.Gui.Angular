b4567bc0fbca76161d3899b0421a9f2a64b9a927
Lisa Malenfant <lisa.malenfant@uci.edu>
2019-11-16 21:29:32 -0800

    Fixed 2 failing angular unit tests and added unit tests for the tissue definition

--- Vts.Gui.Angular/ClientApp/src/app/scatterer-type/scatterer-type.component.spec.ts
+++ Vts.Gui.Angular/ClientApp/src/app/scatterer-type/scatterer-type.component.spec.ts
@@ -1,24 +1,38 @@
 import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
 import { BrowserModule, By } from "@angular/platform-browser";
 import { ScattererTypeComponent } from './scatterer-type.component';
-
-let component: ScattererTypeComponent;
-let fixture: ComponentFixture<ScattererTypeComponent>;
+import { Component, ViewChild, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 describe('scatterer-type component', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [ ScattererTypeComponent ],
-            imports: [ BrowserModule ],
-            providers: [
-                { provide: ComponentFixtureAutoDetect, useValue: true }
-            ]
-        });
-        fixture = TestBed.createComponent(ScattererTypeComponent);
-        component = fixture.componentInstance;
-    }));
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
 
-    it('should do something', async(() => {
-        expect(true).toEqual(true);
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ScattererTypeComponent, TestHostComponent],
+      imports: [FormsModule],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+  }));
+
+  it('Scatter Type should be PowerLaw', async(() => {
+    testHostComponent.scattererTypeComponent.intralipid = { volumeFraction: 0.01, show: false };
+    testHostComponent.scattererTypeComponent.powerLaw = { a: 1.2, b: 1.42, show: true };
+    testHostComponent.scattererTypeComponent.mieParticle = { particleRadius: 0.5, particleN: 1.4, mediumN: 1, volumeFraction: 0.01, show: false };
+    testHostComponent.scattererTypeComponent.scattererType = { value: 'PowerLaw', display: 'PowerLaw [A*λ^(-b)]' };
+    testHostFixture.detectChanges();
+    expect(testHostComponent.scattererTypeComponent.scattererType.value).toEqual('PowerLaw');
+  }));
+
+  @Component({
+    selector: `host-component`,
+    template: `<app-scatterer-type></app-scatterer-type>`,
+  })
+  class TestHostComponent {
+    @ViewChild(ScattererTypeComponent)
+    public scattererTypeComponent: ScattererTypeComponent;
+  }
 });
--- Vts.Gui.Angular/ClientApp/src/app/tissue-definition/tissue-definition.component.spec.ts
+++ Vts.Gui.Angular/ClientApp/src/app/tissue-definition/tissue-definition.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from "@angular/platform-browser";
 import { TissueDefinitionComponent } from './tissue-definition.component';
+import { Liver, Skin } from './absorber-list';
 
 describe('tissue-definition component', () => {
   let testHostComponent: TestHostComponent;
@@ -13,8 +14,7 @@ describe('tissue-definition component', () => {
         declarations: [TissueDefinitionComponent, TestHostComponent],
         imports: [FormsModule],
         schemas: [NO_ERRORS_SCHEMA]
-      })
-        .compileComponents();
+      });
     }));
 
   beforeEach(async(() => {
@@ -24,6 +24,8 @@ describe('tissue-definition component', () => {
       display: "Liver",
       value: "Liver"
     };
+    testHostComponent.tissueDefinitionComponent.absorberConcentration = Liver;
+    testHostComponent.tissueDefinitionComponent.bloodConcentration = { totalHb: 190, bloodVolume: 0.0817, stO2: 0.65, visible: true };
     testHostFixture.detectChanges();
   }));
 
@@ -31,6 +33,19 @@ describe('tissue-definition component', () => {
     testHostFixture.whenStable().then(() => {
       const testElement = testHostFixture.debugElement.query(By.css('#tissue-type'));
       expect(testElement.nativeElement.value).toBe('Liver');
+    });
+  }));
+
+  it('should change the absorber values when tissue type is changed to Skin', async(() => {
+    testHostFixture.whenStable().then(() => {
+      const testElement = testHostFixture.debugElement.query(By.css('#tissue-type'));
+      testElement.nativeElement.value = 'Skin';
+      testElement.nativeElement.dispatchEvent(new Event('change'));
+      testHostFixture.detectChanges();
+      expect(testHostComponent.tissueDefinitionComponent.tissueType.value).toBe('Skin');
+      //expect(testHostComponent.tissueDefinitionComponent.tissueType.display).toBe('Skin');
+      expect(testHostComponent.tissueDefinitionComponent.absorberConcentration).toBe(Skin);
+
     });
   }));
 
